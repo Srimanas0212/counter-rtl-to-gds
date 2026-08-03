f3895e15b99cee152cfec637529d8f518dcf95a0
Srimanas0212 <vsri.manas0212@gmail.com>
2026-08-04 01:57:44 +0530

    Add files via upload

---
+++ sta_eco/hold_optimized.tcl
@@ -0,0 +1,4 @@
+ecoAddRepeater -cell BUFX2  -net rst          -name FE_ECOC0_rst
+ecoAddRepeater -cell BUFX4  -net FE_ECON0_rst  -name FE_ECOC1_rst
+ecoAddRepeater -cell BUFX8  -net n_5           -name FE_ECOC2_n_5
+ecoAddRepeater -cell BUFX12 -net FE_ECON1_rst  -name FE_ECOC3_rst
